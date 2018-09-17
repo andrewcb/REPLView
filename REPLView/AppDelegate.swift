ad2bc8e2f71fa176244270186b654c83dd82371e
acb <acb@staticplain.local>
2018-09-17 01:25:14 +0100

    Initial Commit

---
+++ REPLView/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  REPLView
+//
+//  Created by acb on 17/09/2018.
+//  Copyright © 2018 Kineticfactory. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
