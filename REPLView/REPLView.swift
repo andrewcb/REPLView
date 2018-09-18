6836f80df3526c46f4106bdf2f2415331977dc19
acb <acb@staticplain.local>
2018-09-18 14:59:06 +0100

    Added (local) history handling

--- REPLView/REPLView.swift
+++ REPLView/REPLView.swift
@@ -14,24 +14,62 @@ import Cocoa
 @IBDesignable
 class REPLView: NSView {
     
+    
+    /** The maximum number of history lines to save*/
+    @IBInspectable
+    var maxHistoryLines: Int = 20
+    
     class TextField: NSTextField {
         var submitText: ((String)->())?
+        var handleSpecialKey: ((SpecialKey)->())?
+        
+        enum SpecialKey: UInt16 {
+            case up = 126
+            case down = 125
+        }
         
         override func keyUp(with event: NSEvent) {
-            print("keyUp: keyCode = \(event.keyCode); modifierFlags = \(event.modifierFlags) \(event.modifierFlags.intersection(.deviceIndependentFlagsMask))")
             if event.keyCode == 36 /* Enter */ && event.modifierFlags.intersection(.deviceIndependentFlagsMask).isEmpty {
                 self.submitText?(self.stringValue)
                 self.stringValue = ""
+            } else if let specialKey = SpecialKey(rawValue: event.keyCode) {
+                self.handleSpecialKey?(specialKey)
             } else {
                 super.keyUp(with: event)
             }
         }
     }
     
-    
     var scrollView: NSScrollView = NSScrollView()
     var scrollbackTextView: NSTextView = NSTextView()
     var inputField: TextField = TextField()
+    
+    // history handling
+    var history: [String] = []
+    var currentlyEditedLine: String? = nil // the line being entered at the moment, which the user can return to
+    // where the user is currently in navigating the history (or not)
+    enum HistoryNavigationState: Equatable {
+        case currentLine
+        case historyItem(Int)
+    }
+    var historyNavigationState = HistoryNavigationState.currentLine {
+        didSet(prev) {
+            guard self.historyNavigationState != prev else { return }
+            if prev == .currentLine { self.currentlyEditedLine = self.inputField.stringValue }
+            switch(self.historyNavigationState) {
+            case .currentLine: self.inputField.stringValue = self.currentlyEditedLine ?? ""
+            case .historyItem(let index): self.inputField.stringValue = self.history[index]
+            }
+        }
+    }
+    func addToHistory(line: String) {
+        if self.history.count >= self.maxHistoryLines {
+            self.history.removeFirst(self.history.count - (self.maxHistoryLines - 1))
+        }
+        self.history.append(line)
+    }
+    
+    //MARK: -----
     
     @IBInspectable
     var font: NSFont? {
@@ -70,9 +108,7 @@ class REPLView: NSView {
     }
     
     private func configureSubviews() {
-        // TODO: scroller
         self.addSubview(self.scrollView)
-//        self.scrollView.contentView.addSubview(self.scrollbackTextView)
         self.scrollView.documentView = self.scrollbackTextView
         self.scrollView.borderType = .noBorder
         self.scrollView.hasVerticalScroller = true
@@ -84,9 +120,12 @@ class REPLView: NSView {
         self.inputField.maximumNumberOfLines = 0
         self.inputField.submitText = { line in
             self.println(line)
+            self.addToHistory(line: line)
+            self.historyNavigationState = .currentLine
         }
         
-//        self.scrollbackTextView.string.append("Hello world!\n")
+        self.inputField.handleSpecialKey = self.handleInputSpecialKey
+        
         self.scrollbackTextView.isEditable = false
         
         self.needsLayout = true
@@ -102,7 +141,6 @@ class REPLView: NSView {
         super.layout()
         self.inputField.stringValue = self.inputField.stringValue
         let inputTextSize = self.inputField.sizeThatFits(self.frame.size)
-//        print("\(inputField.stringValue) -> \(inputTextSize)")
         self.inputField.preferredMaxLayoutWidth = self.frame.width
         self.inputField.frame = NSRect(x: 0.0, y: 0.0, width: self.frame.width, height: max(inputTextSize.height, 24.0))
         
@@ -112,8 +150,26 @@ class REPLView: NSView {
             let textSize = layoutManager.usedRect(for: textContainer)
             self.scrollView.frame = NSRect(x: 0.0, y: self.inputField.frame.height, width: self.frame.width, height: min(self.frame.height - self.inputField.frame.height, textSize.height))
             self.scrollbackTextView.frame = NSRect(x: 0.0, y: 0.0, width: self.scrollView.frame.width, height: textSize.height)
-//            self.scrollView.documentView?.frame = NSRect(origin: .zero, size: textSize.size)
-//            self.scrollbackTextView.frame = NSRect(x: 0.0, y: self.inputField.frame.height, width: textSize.width, height: textSize.height)
+        }
+    }
+    
+    func handleInputSpecialKey(_ key: TextField.SpecialKey) {
+        switch(key) {
+        case .up:
+            if !self.history.isEmpty {
+                switch(self.historyNavigationState) {
+                case .currentLine:
+                    self.historyNavigationState = .historyItem(self.history.count - 1)
+                case .historyItem(let item):
+                    if item > 0 {
+                        self.historyNavigationState = .historyItem(item - 1)
+                    }
+                }
+            }
+        case .down:
+            if case let .historyItem(item) = self.historyNavigationState {
+                self.historyNavigationState = (item < self.history.count - 1) ? .historyItem(item+1) : .currentLine
+            }
         }
     }
     
